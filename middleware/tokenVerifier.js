ce1c880654da3f0b1c49df36c72c27750f3c51bc
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-11-24 14:53:40 -0500

    wip

---
+++ middleware/tokenVerifier.js
@@ -0,0 +1,30 @@
+/**
+ * verifies if the toke provided by the user is valid
+ */
+
+/* imports */
+
+const jwt = require('../utils/jwt');
+
+/* middleware */
+
+/**
+ * verfies if a token is valid
+ *
+ * @param request
+ * @param response
+ * @param next
+ */
+const tokenVerifier = (request, response, next) => {
+  const token = request.get('authorization');
+
+  if (!token || !jwt.verifyToken(token)) {
+    response.status(403).end();
+    return;
+  }
+  next();
+};
+
+/* exports */
+
+module.exports = tokenVerifier;
---
+++ utils/mailer.js
@@ -0,0 +1,99 @@
+/**
+ * mailer functions
+ */
+
+/* imports*/
+
+const nodemailer = require('nodemailer');
+const {google} = require('googleapis');
+const config = require('../config/config');
+
+/* setup */
+
+// fill client id and client secret
+
+const oAuth = new google.auth.OAuth2(
+  '377525267557-ous157vpt3p00bbpe52dufm54gestri8.apps.googleusercontent.com',
+  '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',
+  config.REDIRECT_URI
+);
+oAuth.setCredentials({ refresh_token: config.REFRESH_TOKEN });
+
+/* functions */
+
+/**
+ * sends an email
+ *
+ * @param to
+ * @param cc
+ * @param bcc
+ * @param subject
+ * @param text
+ * @param html
+ * @returns {Promise<*|*>}
+ */
+const send = async (to, cc, bcc, subject, text, html) => {
+
+  try {
+
+    // fill client id and client secret
+
+    const transport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'oauth2',
+        user: 'tododorotimer@gmail.com',
+        clientId: '377525267557-ous157vpt3p00bbpe52dufm54gestri8.apps.googleusercontent.com',
+        clientSecret: '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',
+        refreshToken: config.REFRESH_TOKEN,
+        accessToken: oAuth.getAccessToken()
+      }
+    });
+
+    const options = {
+      from: 'TodoDoro: Productivity Todo List',
+      to,
+      cc,
+      bcc,
+      subject,
+      text,
+      html,
+    };
+    return await transport.sendMail(options);
+
+  } catch (e) {
+    return e;
+  }
+};
+
+/**
+ * sends an email with the verification code
+ *
+ * @param to
+ * @param code
+ * @returns {Promise<void>}
+ */
+const sendVerificationCode = async (to, code) => {
+  const emailBody = `
+  Hello from TodoDoro!
+  
+  We are happy to have you with us! Please use the following code to verify your account:
+  
+  ${code}
+  
+  Please note the code expires in 24 hours.
+  `;
+
+  return await send(to, null, null, 'Verify your new TodoDoro account', emailBody, null);
+};
+
+send('otaviopiza27@gmail.com', null, null, 'feliz aniversarion', 'iai cara feliz aniversarion', null).then(res => {
+  console.log(res);
+});
+
+/* exports */
+
+module.exports = {
+  send,
+  sendVerificationCode
+};
