a1f1fdedc426235487d593bab0f4d4dddf18db9f
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-11-24 14:53:57 -0500

    wip

--- config/config.js
+++ config/config.js
@@ -15,5 +15,9 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT,
   MONGODB_URI: process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI,
-  SECRET: process.env.SECRET
+  SECRET: process.env.SECRET,
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  REDIRECT_URI: process.env.REDIRECT_URI,
+  REFRESH_TOKEN: process.env.REFRESH_TOKEN
 };
